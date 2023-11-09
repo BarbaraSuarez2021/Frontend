f5d5705d759523aa25e372971984a5b8cb8a5e77
ADiegoB <145626297+ADiegoB@users.noreply.github.com>
2023-11-09 14:31:39 -0500

    Delete frontDiego/FH_front directory

--- frontDiego/FH_front/src/app/models/Usuario.ts
+++
@@ -1,9 +0,0 @@
-export interface Usuario{
-        idU: number,
-        idTipoDonacion: number,
-        idRoles: number,
-        donacionU: string,
-        contrasena: string,
-        apelidoU: string,
-        nombreU: string
-}
